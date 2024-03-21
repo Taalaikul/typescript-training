277e25b87635f274bfbdcce440e43d2e2c0c0667
TAALAIKUL MELETIOU <92232851+Taalaikul@users.noreply.github.com>
2024-03-20 23:13:50 -0400

    Inheritance

--- 05-inheritance/Shape.ts
+++ 05-inheritance/Shape.ts
@@ -1,8 +1,27 @@
 
 export class Shape{
 
-    private -x: number;
-    private _y: number;
+    constructor(private _x: number, private _y: number){
+
+    }
+    
+    public get y(): number {
+        return this._y;
+    }
+    public set y(value: number) {
+        this._y = value;
+    }
+
+    public get x(): number {
+        return this._x;
+    }
+    public set x(value: number) {
+        this._x = value;
+    }
+
+    getInfo(): string {
+        return `x=${this._x}, y=${this._y}`;
+    }
 
 
 }
